62a00235f184c7090ab98c14fd7947322cb9e8eb
TIRUMALA KRISHNA MOHAN G <krishna.mohan152@gmail.com>
2020-02-29 21:23:48 +0530

    Add files via upload

---
+++ reverse_cipher.py
@@ -0,0 +1,5 @@
+input = input("Enter the string is: ")
+Reverse_String = input[::-1]
+Dec_Str = Reverse_String[::-1]
+print("The Encoded String is: "+Reverse_String)
+print("The Decoded String is: "+Dec_Str)
